4be01303e547a079085b1c65dddcd9cd413f194e
stackprobe <stackprobes@gmail.com>
2020-08-21 10:01:50 +0900

    Juliet

--- DDnsClient/DDnsClient/MainWin.Designer.cs
+++ DDnsClient/DDnsClient/MainWin.Designer.cs
@@ -108,7 +108,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(284, 261);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Location = new System.Drawing.Point(-400, -400);
 			this.MaximizeBox = false;
